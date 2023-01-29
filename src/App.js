fa990d60010f7f3a695c14b727fe16f7fa10ad09
FouadALLAOUI <fouadallaoui70@gmail.com>
2023-01-29 16:00:28 +0100

    ......

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Users from './components/users/Users';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to RIO</h1>
-      <a href='' className='btn btn-primary'>Click</a>
       <Users/>
     </div>
   );
